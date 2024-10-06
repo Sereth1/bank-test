1c68f1b9012ad4bfac22978adaa66b205e0bfd08
Sereth1 <papakias444@gmail.com>
2024-10-06 16:15:04 +0800

    remove typos

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,15 +2,17 @@
 import React, { useState } from 'react';
 import TransactionForm from '@/components/forms/TransactionForm';
 import Options from '@/components/dashboard/Options';
+import TransactionsList from '@/components/common/TransactionsList';
 
 export default function Page() {
     // Make sure this type matches everywhere
     const [method, setMethod] = useState<'' | 'Deposit' | 'Withdrawal' | 'Transfer'>('');
-
+    const [isOpen, setIsOpen] = useState<boolean>(false)
     return (
         <div>
-            <Options setMethod={setMethod} />
+            <Options setMethod={setMethod} setIsOpen={setIsOpen} isOpen={isOpen} />
             {method !== '' && <TransactionForm transactionType={method} setMethod={setMethod} />}
+            {isOpen && <TransactionsList />}
         </div>
     );
 }
--- components/dashboard/Options.tsx
+++ components/dashboard/Options.tsx
@@ -1,17 +1,18 @@
 interface OptionsProps {
     setMethod: React.Dispatch<React.SetStateAction<'Deposit' | 'Withdrawal' | 'Transfer' | ''>>;
+    setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    isOpen: boolean;
 }
 
-const Options: React.FC<OptionsProps> = ({ setMethod }) => {
+const Options: React.FC<OptionsProps> = ({ setMethod, setIsOpen, isOpen }) => {
     return (
-        <div>
+        <div className='flex justify-evenly p-2 m-10 mt-56 border rounded-xl'>
             <button onClick={() => setMethod('Deposit')}>Deposit</button>
             <button onClick={() => setMethod('Withdrawal')}>Withdraw</button>
             <button onClick={() => setMethod('Transfer')}>Transfer</button>
-            <button onClick={() => setMethod('')}>Reset</button>
+            <button onClick={() => setIsOpen(!isOpen)}>TransactionList</button>
         </div>
     );
 };
 
-
-export default Options
+export default Options;
