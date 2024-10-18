4c9ba70011404dadcc324502aa142001f102f6c9
Nikolaos_X_Chatzinikolaou <129552184+Sereth1@users.noreply.github.com>
2024-10-18 11:50:32 +0800

    Update database.ts

--- utils/database.ts
+++ utils/database.ts
@@ -5,7 +5,7 @@ import mysql from 'mysql2';
 const pool = mysql.createPool({
     host: '202.61.224.164',
     user: 'n1ghtw0olf',
-    password: 't3r4st10s',
+    password: 'H3g7!Zp9Qw2L',
     database: 'banking_app',
     connectionLimit: 10,
     queueLimit: 0,
